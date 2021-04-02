0ad2ae34dc17ee1499ca2d16517910b970666429
moohooooo <suwichalala@gmail.com>
2021-04-02 23:41:42 +0700

    init

--- theme.config.js
+++ theme.config.js
@@ -1,11 +1,12 @@
 export default {
-  repository: 'https://github.com/shuding/nextra',
-  titleSuffix: ' – Nextra',
+  repository: 'https://github.com/ZooFarming/zoo-keeper-docs',
+  titleSuffix: ' – ZooKeeper Docs',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <img src="zoo32x32.png"/>
+      <span className="mr-2 font-extrabold hidden md:inline">ZOOKEEPER</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next Site Builder
+        DOCUMENTS
       </span>
     </>
   ),
@@ -25,17 +26,15 @@ export default {
       <meta name="og:image" content="https://nextra.vercel.app/og.png" />
       <meta name="apple-mobile-web-app-title" content="Nextra" />
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png"/>
-      <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png"/>
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-      <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png"/>
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+      
+      <link rel="icon" type="image/png" href="/favicon.png"/>
       <meta name="msapplication-TileImage" content="/ms-icon-144x144.png"/>
     </>
   ),
   search: true,
   prevLinks: true,
   nextLinks: true,
-  footer: true,
+  footer: false,
   footerEditOnGitHubLink: true,
   footerText: <>MIT {new Date().getFullYear()} © Nextra.</>
 }

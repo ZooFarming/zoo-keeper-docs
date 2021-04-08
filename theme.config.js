e628b85e58f9d6dfc7ffdf896487a2e8e3536d5c
moohooooo <suwichalala@gmail.com>
2021-04-08 18:00:55 +0700

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -35,7 +35,7 @@ export default {
   i18n: [
 	  { locale: 'en', text: 'English' },
 	  { locale: 'zh', text: '中文' },
-	  //{ locale: 'fr', text: 'Français' },
+	  { locale: 'fr', text: 'Français' },
 	],
   search: true,
   prevLinks: true,

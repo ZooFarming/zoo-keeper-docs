da71d6903b24539a71559321008bf013105830d3
moohooooo <suwichalala@gmail.com>
2021-09-27 13:04:11 +0700

    fix missing tag

--- theme.config.js
+++ theme.config.js
@@ -34,7 +34,7 @@ export default {
   ),
   i18n: [
 	  { locale: 'en', text: 'English' },
-	  //{ locale: 'zh', text: '中文' },
+	  /*{ locale: 'zh', text: '中文' },*/
 	  { locale: 'fr', text: 'Français' },
 	  { locale: 'tr', text: 'Türkçe' },
 	],
